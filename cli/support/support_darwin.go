78683972feabc12ac88a707f4d16a537765dba18
Anton Novojilov <andy@essentialkaos.com>
2023-03-08 23:23:53 +0300

    Code refactoring

--- cli/support/support_darwin.go
+++ cli/support/support_darwin.go
@@ -22,29 +22,10 @@ func showOSInfo() {
 		return
 	}
 
-	osInfo, err := system.GetOSInfo()
-
-	if err == nil {
-		fmtutil.Separator(false, "OS INFO")
-
-		printInfo(12, "Name", osInfo.Name)
-		printInfo(12, "Version", osInfo.VersionID)
-		printInfo(12, "Build", osInfo.Build)
-	}
-
 	fmtutil.Separator(false, "SYSTEM INFO")
 
 	printInfo(7, "Name", systemInfo.OS)
+	printInfo(7, "Version", systemInfo.Version)
 	printInfo(7, "Arch", systemInfo.Arch)
 	printInfo(7, "Kernel", systemInfo.Kernel)
-}
-
-// collectEnvInfo collects info about environment
-func collectEnvInfo() Pkgs {
-	return nil
-}
-
-// showEnvInfo shows info about environment
-func showEnvInfo(pkgs Pkgs) {
-	return
 }
