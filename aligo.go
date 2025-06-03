796da2df3fe7e3fd992e0d6929161864dba9a582
Anton Novojilov <andy@essentialkaos.com>
2025-06-03 13:47:03 +0300

    Update copyright header

--- aligo.go
+++ aligo.go
@@ -2,7 +2,7 @@ package main
 
 // ////////////////////////////////////////////////////////////////////////////////// //
 //                                                                                    //
-//                         Copyright (c) 2024 ESSENTIAL KAOS                          //
+//                         Copyright (c) 2025 ESSENTIAL KAOS                          //
 //      Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>     //
 //                                                                                    //
 // ////////////////////////////////////////////////////////////////////////////////// //
